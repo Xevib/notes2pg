e4d4385cb1fe1726f654e60dc6b4cb88c28e08d9
Xavier Barnada <xbarnada@gmail.com>
2016-02-04 21:59:04 +0100

    Improve schema

--- schema.sql
+++ schema.sql
@@ -1,7 +1,14 @@
 CREATE TABLE planet_osm_notes(
-	id integer NOT NULL,
+	id integer NOT NULL PRIMARY KEY,
 	geom geometry NOT NULL,
 	created_at timestamp,
 	closed_at timestamp
 );
-
+CREATE TABLE planet_osm_notes_comments(
+	note_id integer REFERENCES planet_osm_notes (id),
+	action text,
+	"timestamp" timestamp,
+	uid integer,
+	"user" text,
+	comment text
+);
